35ad8adaf4196fbdb1562805e71c20beedec7630
Erik Brakkee <erik@brakkee.org>
2022-07-26 20:52:17 +0200

    reference to stackoverflow added.

--- policytester/SafeLineLoader.py
+++ policytester/SafeLineLoader.py
@@ -1,6 +1,10 @@
 import yaml
 
 class SafeLineLoader(yaml.SafeLoader):
+    """
+    Parse yaml with line number added as __line__ attributes to the dictionaries.
+    See https://stackoverflow.com/questions/13319067/parsing-yaml-return-with-line-number
+    """
     def construct_mapping(self, node, deep=False):
         mapping = super(SafeLineLoader, self).construct_mapping(node, deep=deep)
         # Add 1 so line numbering starts at 1
